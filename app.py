37fc9027f126a4b3b2f79160ebaad4573ae99e84
Ruchi Yadav <ruchii22yadav22@gmail.com>
2023-10-18 12:26:35 +0530

    india visualization project using plotly and streamlit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(layout='wide')
+
+df=pd.read_csv('india.csv')
+list_of_states=list(df['State'].unique())
+list_of_states.insert(0,'Overall India')
+st.sidebar.title('India ka visualization')
+selected_state=st.sidebar.selectbox('Select a state',list_of_states)
+primary=st.sidebar.selectbox('Select a primary parameter',sorted(df.columns[5:]))
+secondary=st.sidebar.selectbox('Select a secondary parameter',sorted(df.columns[5:]))
+
+plot=st.sidebar.button("Plot Graph")
+
+if plot:
+    st.text("size represent primary parameter")
+    st.text("color represent secondary parameter")
+    if selected_state == 'Overall India':
+        fig=px.scatter_mapbox(df, lat='Latitude', lon='Longitude', zoom=3,
+                              size=primary,color=secondary, mapbox_style='carto-positron',
+                              width=1200,height=700,hover_name='District')
+        
+        st.plotly_chart(fig,use_container_width=True)
+    else:
+        state_df=df[df['State'] == selected_state]
+        fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude', zoom=3,
+                                size=primary, color=secondary, mapbox_style='carto-positron',
+                                width=1200, height=700,hover_name='District')
+
+        st.plotly_chart(fig, use_container_width=True)
+
